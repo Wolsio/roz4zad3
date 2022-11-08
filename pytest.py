9fdff783677194bdae6e9a8a9c5c7b4da0073053
Wolsio <116030222+Wolsio@users.noreply.github.com>
2022-11-08 11:18:38 +0100

    Update pytest.py

--- pytest.py
+++ pytest.py
@@ -1,15 +1,6 @@
 import pytest
 
 
-def test_header_is_uppercase():
-    """Check if column names in header are uppercase"""
-    assert True
-
-def test_record_matches_header():
-    """Check if number of columns in each record matches header"""
-    assert True
-
-
-def test_record_first_field_is_number():
-    """Check if the first value in each record is a number"""
+def element_in_list_are_numbers():
+    """Check if elements in list are numbers"""
     assert True
